ce1b5dbd39a8cca64824e72da119297a02205700
Alex Tang <lalextang@gmail.com>
2012-06-30 23:30:33 +0800

    add open source charting component projects

---
+++ src/PlotCharts/PlotCharts/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace PlotCharts
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rdn = new Random();
+            for (int i = 0; i < 50; i++)
+            {
+                chart1.Series["test1"].Points.AddXY(rdn.Next(0,10), rdn.Next(0,10));
+                chart1.Series["test2"].Points.AddXY(rdn.Next(0,10), rdn.Next(0,10));
+            }
+                        
+            chart1.Series["test1"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["test1"].Color = Color.Red;
+
+            chart1.Series["test2"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["test2"].Color = Color.Blue;           
+            
+        }
+    }
+}
